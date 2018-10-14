fe9d0dde71145fcc4ca5a7beaa1fff48727f51d2
Ben <ben@benstidham.com>
2018-10-14 17:49:48 -0500

    Complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { Provider } from "react-redux";
+import store from "./store";
 
-import Counter from './Counter';
+import Counter from "./Counter";
 
 class App extends Component {
   render() {
-    return <Counter />;
+    return (
+      <Provider store={store}>
+        <Counter />
+      </Provider>
+    );
   }
 }
 
